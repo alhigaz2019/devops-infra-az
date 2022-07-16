2f4b3ef766ef05f80ba408f7e09efcb35c12a6c7
alhigaz2019 <alhigaz2019@gmail.com>
2022-07-16 02:42:31 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "azurerm" {
     resource_group_name  = "tf-state"
-    storage_account_name = "sasanthoshmail2utfstate"
+    storage_account_name = "alhigaz2019gmail2tfstate"
     container_name       = "tf-state"
     key                  = "terraform.tfstate"
   }
